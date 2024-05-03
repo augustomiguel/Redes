bd64c83413475f888a9467255b9fbbca6295725a
augustomiguel <augusto.miguel@academico.ufpb.br>
2024-05-03 12:46:07 -0300

    menu

--- main.py
+++ main.py
@@ -3,30 +3,37 @@ from clienteUDP import udp
 
 
 def main():
+    aux=[1,2,3,4]
     while True:
         print("Digite qual solicitação ira ser realizada")
         print("1. Data e hora atual;")
         print("2. Uma mensagem motivacional para o fim do semestre;")
         print("3. A quantidade de respostas emitidas pelo servidor até o momento.")
         print("4. Sair")
-        solicitacao=input(">")
-        
-        print("\nDigite qual requisição vc quer fazer\n")
-        print("1. Requisição UDP ")
-        print("2. Requisição RAW")
-        print("3. Sair")
-
-        opcao = input("\n> ")
-
-        if opcao == "1":
-            udp()
-        elif opcao == "2":
-            raw()
-        elif opcao == "3":
-            print("\nencerrando programa")
-            break
+        solicitacao = int(input(">"))
+        if solicitação not in aux:
+            continue
+        elif solicitacao == 4 :
+            print("encerrando programa")
+            exit(0)
         else:
-            print("opcao inválida")
+            while True:
+                print("\nDigite qual requisição vc quer fazer\n")
+                print("1. Requisição UDP ")
+                print("2. Requisição RAW")
+                print("3. Sair")
+
+                opcao = input("\n> ")
+
+                if opcao == "1":
+                    udp(solicitacao)
+                elif opcao == "2":
+                    raw(solicitacao)
+                elif opcao == "3":
+                    print("\nsaindo")
+                    break
+                else:
+                    print("opcao inválida")
 
 
 
