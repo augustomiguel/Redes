26c4507126308d890de8253ee23e75fba187885d
MatheEduar <edutheu_dev@hotmail.com>
2024-05-05 15:28:55 -0300

    Me matando para entender conversão em python

--- clienteUDP.py
+++ clienteUDP.py
@@ -1,30 +1,15 @@
 from socket import *
-import struct
-from mensagem import MensagemUDP
 
+class SocketUDP:
 
-serverName = "15.228.191.109"
-serverPort = 50000
-processar_mensagem = Mensagem()
+    def __init__(self):
+        self.serverName = '15.228.191.109'
+        self.serverPort = 50000
+        self.clientSocket = socket(AF_INET, SOCK_DGRAM)
+        self.resposta = ""
+        self.serverAddress = ""
 
-def udp (self,opcao):
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    message = opcao
-
-    clientSocket.sendto(message.encode(),(serverName, serverPort))
-    enviar_mensagem()
-    sen
-
-    print(modifiedMessage.decode())
-    clientSocket.close()
-    
-
-def enviar_mensagem(socket, message, server_address):
-    # Convert message to MensagemUDP object
-    mensagem_udp = MensagemUDP(message.tipo, message.payload)
-
-    # Convert MensagemUDP object to bytes
-    message_bytes = mensagem_udp.to_bytes()
-
-    # Send the message to the server
-    socket.sendto(message_bytes, server_address)
+    def mandar_mensagem(self, mensagem):
+        self.clientSocket.sendto(mensagem.encode(), (self.serverName,self.serverPort))
+        self.resposta, self.serverAddress = self.clientSocket.recvfrom(2048)
+        self.clientSocket.close()
--- main.py
+++ main.py
@@ -1,40 +1,62 @@
-from clienteUDP import udp
-#from clienteRAW import raw
+from socket import *
+from clienteUDP import SocketUDP
+import binascii
+import random
 
-def main():
-    aux=[1,2,3,4]
-    while True:
-        print("Digite qual solicitação ira ser realizada")
-        print("1. Data e hora atual;")
-        print("2. Uma mensagem motivacional para o fim do semestre;")
-        print("3. A quantidade de respostas emitidas pelo servidor até o momento.")
-        print("4. Sair")
-        solicitacao = int(input(">"))
-        if solicitacao == 4 :
-            print("encerrando programa")
-            exit(0)
-        elif solicitacao not in aux:
-            continue
-        else:
-            while True:
-                print("\nDigite qual requisição vc quer fazer\n")
-                print("1. Requisição UDP ")
-                print("2. Requisição RAW")
-                print("3. Sair")
+data = "\x00"
+frase = "\x01"
+quantidade = "\x02"
+requisicao_invalida = "\x03"
 
-                opcao = input("\n> ")
+mensagem_enviada = ""
+mensagem_recebida = ""
 
-                if opcao == "1":
-                    udp(solicitacao)
-                elif opcao == "2":
-                    #raw(solicitacao)
-                    pass
-                elif opcao == "3":
-                    print("\nsaindo")
-                    break
-                else:
-                    print("opcao inválida")
+serverName = "15.228.191.109"
+serverPort = 50000
+
+while(True):
+    print("O que você deseja fazer? ")
+    print("1) Solicitar Data e Hora atual.")
+    print("2) Uma mensagem motivacional para o fim do semestre.")
+    print("3) A quantidade de respostas emitidas pelo servidor até o momento.")
+    print("4) Sair.")
+    opcao = input("-> ")
+    if opcao == "1":
+        mensagem_enviada += data
+        random_number = random.randint(1, 65535)
+        identificador = hex(random_number)
+        
+        # Remove o '0b' e preenche os números a esquerda até que tenham 16 dígitos
+        identificador = identificador[2:].zfill(16)  
+        primeiro_4bits = int(identificador[:4],16)
+        segundo_4bits = int(identificador[4:8],16)
+        terceiro_4bits = int(identificador[8:12],16)
+        quarto_4bits = int(identificador[12:16],16)
+        
+        print(f'''identificador: {identificador}\n
+        primeiro 4bits: {primeiro_4bits}\n
+        segundo 4bits: {segundo_4bits}\n
+        terceiro 4bits: {terceiro_4bits}\n
+        quarto 4bits: {quarto_4bits}\n
+        ''')
 
 
+        mensagem_enviada += identificador
+       
 
-main()
+        udp = SocketUDP()
+        udp.mandar_mensagem("\x00\x00\x11")
+        mensagem_recebida = udp.resposta
+        print(mensagem_recebida)
+        mensagem_enviada = ""
+
+
+    elif opcao == "2":
+        pass
+    elif opcao == "3":
+        pass
+    elif opcao == "4":
+        print("Programa finalizado.")
+        exit(0)
+    else:
+        print("Opcao inválida!")
