ee736651df71c9d771ec09700a36129fddbc57d0
MatheEduar <edutheu_dev@hotmail.com>
2024-05-06 09:13:32 -0300

    UDP finalizado bonitinho

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ serverPort = 50000
 mensagem_enviada = b""
 
 def enviar_mensagem(opcao):
-    data = "\x00"
-    frase = "\x01"
-    quantidade = "\x02"
-    requisicao_invalida = "\x03"
+    data = b"\x00"
+    frase = b"\x01"
+    quantidade = b"\x02"
+    requisicao_invalida = b"\x03"
 
-    mensagem_enviada = ""
+    mensagem_enviada = b""
     if opcao == "1":
         mensagem_enviada += data
     elif opcao == "2":
@@ -29,10 +29,26 @@ def enviar_mensagem(opcao):
         mensagem_enviada += quantidade
     elif opcao == "4":
         exit(0)
-    
+    else:
+        print("Opção inválida!")
+        return
+
     random_number = random.randint(1, 65535)
     identificador = random_number.to_bytes(length=2, byteorder="big")
     mensagem_enviada += identificador
+
+    udp = SocketUDP()
+    udp.mandar_mensagem(mensagem_enviada)
+    mensagem_recebida = udp.resposta
+    if opcao == "1" or opcao == "2":
+        mensagem_recebida = mensagem_recebida[4:-2]
+        mensagem_recebida = mensagem_recebida.decode("utf-8")
+    elif opcao == "3":
+        mensagem_recebida = mensagem_recebida[-4:]
+        mensagem_recebida = int.from_bytes(mensagem_recebida, byteorder='big', signed=False)
+    print(mensagem_recebida)
+    
+    return
 
 
 
@@ -43,80 +59,4 @@ while(True):
     print("3) A quantidade de respostas emitidas pelo servidor até o momento.")
     print("4) Sair.")
     opcao = input("-> ")
-    if opcao == "1":
-        mensagem_enviada += data
-
-        # Gera um número de 1 a 65535 
-        random_number = random.randint(1, 65535)
-        
-        identificador = random_number.to_bytes(length=2, byteorder="big")
-        print(mensagem_enviada)
-        mensagem_enviada += identificador
-        print(mensagem_enviada)
-       
-        # Envia a mensagem 
-        udp = SocketUDP()
-        udp.mandar_mensagem(mensagem_enviada)
-        mensagem_recebida = udp.resposta
-        mensagem_recebida = mensagem_recebida[4:-2]
-        mensagem_recebida = mensagem_recebida.decode("utf-8")
-        print(mensagem_recebida)
-        mensagem_enviada = ""
-
-
-    elif opcao == "2":
-        mensagem_enviada += frase
-        # Gera um número de 1 a 65535 
-        random_number = random.randint(1, 65535)
-        # Transforma o número em hexadecimal
-        identificador = hex(random_number)
-        # Remove o '0b'
-        identificador = identificador[2:]
-        # utiliza os primeiros 4 bits do número
-        primeiro_byte = "\\x" + identificador[:2]
-        # utiliza os 4 últimos bits do número
-        segundo_byte = "\\x" + identificador[2:4]
-
-        # Adiciona o número na mensagem
-        mensagem_enviada += primeiro_byte
-        mensagem_enviada += segundo_byte
-       
-        # Envia a mensagem 
-        udp = SocketUDP()
-        udp.mandar_mensagem(mensagem_enviada)
-        mensagem_recebida = udp.resposta
-        mensagem_recebida = mensagem_recebida[4:-2]
-        mensagem_recebida = mensagem_recebida.decode("utf-8")
-        print(mensagem_recebida)
-        mensagem_enviada = ""
-    elif opcao == "3":
-        mensagem_enviada += quantidade
-
-        # Gera um número de 1 a 65535 
-        random_number = random.randint(1, 65535)
-        # Transforma o número em hexadecimal
-        identificador = hex(random_number)
-        # Remove o '0b'
-        identificador = identificador[2:]
-        # utiliza os primeiros 4 bits do número
-        primeiro_byte = "\\x" + identificador[:2]
-        # utiliza os 4 últimos bits do número
-        segundo_byte = "\\x" + identificador[2:4]
-
-        # Adiciona o número na mensagem
-        mensagem_enviada += primeiro_byte
-        mensagem_enviada += segundo_byte
-       
-        # Envia a mensagem 
-        udp = SocketUDP()
-        udp.mandar_mensagem(mensagem_enviada)
-        mensagem_recebida = udp.resposta
-        mensagem_recebida = mensagem_recebida[-4:]
-        mensagem_recebida = int.from_bytes(mensagem_recebida, byteorder='big', signed=False)
-        print(mensagem_recebida)
-        mensagem_enviada = ""
-    elif opcao == "4":
-        print("Programa finalizado.")
-        exit(0)
-    else:
-        print("Opcao inválida!")
+    enviar_mensagem(opcao)
