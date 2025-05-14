a518045bbf7037ab08321c8b41d84f1aaa0af09f
Augusto Miguel <augusto122232@gmail.com>
2025-05-14 12:37:36 -0300

    .

--- clienteRAW.py
+++ clienteRAW.py
@@ -11,22 +11,24 @@ endereco_binario = struct.pack('!BBBB', *ip_numerico)
 
 address = "15228191109"
 serverPort = 50000
-server_address = (endereco_binario, serverPort)
+server_address = (ip_servidor, serverPort)
 
 class SocketRAW:
 
     def send_message(self, mensagem):
-        with self.criar_raw_socket() as socket:  # Usa o gerenciador de contexto para fechamento automático de socket
-            # Envia a mensagem para o servidor
-            print("enviando a mensagem: {0} para o endereço: {1}".format( mensagem, server_address))
-            socket.sendto(mensagem, (ip_servidor,serverPort))
+        try:
+            with self.criar_raw_socket() as socket:  # Usa o gerenciador de contexto para fechamento automático de socket
+                # Envia a mensagem para o servidor
+                print("enviando a mensagem: {0} para o endereço: {1}".format( mensagem, server_address))
+                socket.sendto(mensagem, (ip_servidor,serverPort))
 
-            # Receba a resposta (assumindo que o servidor envie uma resposta)
-            print("esperando resposta")
-            mensagem_recebida, _ = socket.recvfrom(4096)  
-            print(mensagem_recebida)
-            return mensagem_recebida
-
+                # Receba a resposta (assumindo que o servidor envie uma resposta)
+                print("esperando resposta")
+                mensagem_recebida, _ = socket.recvfrom(4096)  
+                print(mensagem_recebida)
+                return mensagem_recebida
+        except OSError as e:
+            raise OSError(f"Failed to create raw socket: {e}")
 
     def criar_raw_socket(self):
         try:
